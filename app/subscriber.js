336510f1e0ae6c4116b8c3938c3586a362c030e0
rcmonitor <rcmonitor@mail.ru>
2015-03-07 10:06:41 +0300

    make subscription works
    make use of helpers package

---
+++ app/subscriber.js
@@ -0,0 +1,71 @@
+/**
+ * //@by_rcmonitor@//
+ * on 05.03.2015.
+ */
+
+var amqp = require('amqp');
+var cEventEmitter = require('events').EventEmitter;
+
+var amqpHacks = require('./amqp_hacks.js');
+var hpg = require('helpers-global');
+
+var oExchangeParameters = {
+	"type": "topic"
+	, "autoDelete": false
+	, "durable": true
+	, "confirm": true
+};
+
+var oQueueParameters = {
+	"passive": false,
+	"durable": false,
+	"exclusive": true,
+	"autoDelete": false
+};
+
+var oDeployer = new cEventEmitter();
+//var oDate = new Date();
+
+oDeployer.validEvents = {
+	"deployed": "deployed"
+};
+
+var timeStarted = new Date().getTime();
+
+oDeployer.wait = function(oRabbitProperties){
+
+	amqpConnection = amqp.createConnection(oRabbitProperties);
+
+	amqpConnection.on('ready', function(){
+
+		hpg.log('connection established in ' + hpg.diff(timeStarted));
+
+		amqpConnection.exchange('parser_exchange', oExchangeParameters, function(amqpExchange){
+
+			hpg.log('exchange validated on ' + hpg.diff(timeStarted));
+
+			amqpConnection.queue('parsed_queue', oQueueParameters, function(amqpQueue){
+
+				hpg.log('queue created on ' + hpg.diff(timeStarted) + ' and callback fired');
+
+				amqpQueue.bind('parser_exchange', 'parser.out', function(){
+
+					hpg.log('queue bounded on ' + hpg.diff(timeStarted) + ' waiting for messages');
+
+					var intMsgCount = 0;
+					amqpQueue.subscribe(function(msg){
+						intMsgCount ++;
+						hpg.log(msg.data.toString('utf-8'), 'message #' + intMsgCount);
+					});
+				});
+
+			});
+
+		});
+
+	});
+
+};
+
+
+module.exports = oDeployer;
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /**
  * Created by jack on 05.03.2015.
  */
+
+oDeployer = require('./app/subscriber');
+
+$oRabbitProperties = {
+	"host": 'localhost'
+};
+
+oDeployer.wait($oRabbitProperties);
